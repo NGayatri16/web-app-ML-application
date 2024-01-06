51a1bb0590d8c2142c3927f20c21f5559c1b9b9d
NGayatri16 <155796582+NGayatri16@users.noreply.github.com>
2024-01-06 12:45:15 +0530

    created web app model

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ X=dataset.iloc[:,:-1].values
 Y=dataset.iloc[:,-1].values
 
 X_train,X_test,Y_tain,Y_test=train_test_split(X,Y,test_size=0.2)
+
 classifier=LogisticRegression()
 classifier.fit(X_train,Y_train)
 
-st.title('Crop Recommundation')
-n=st.number_input('Enter Nitrogen')
-p=st.number_input('Enter phosphrous')
-k=st.number_input('Enter potassium')
-t=st.number_input('Enter Temperature')
-h=st.number_input('Enter Humidity')
-ph=st.number_input('Enter Number')
-r=st.number_input('Enter Rainfall')
+st.title('Crop Recommendation')
+n=st.number_input('Enter Nitrogen:')
+p=st.number_input('Enter Phosphrous:')
+k=st.number_input('Enter Potassium:')
+t=st.number_input('Enter Temperature:')
+h=st.number_input('Enter Humidity:')
+ph=st.number_input('Enter PH:')
+r=st.number_input('Enter Rainfall:')
 
 if st.button('Recommend Crop'):
     data=[[n,p,k,t,ph,r]]
